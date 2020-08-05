798176b7cd71fe7e3f77ed6d92fcd49c73e0fde2
Dorcas Blutty <bluttydorcas@gmail.com>
2020-08-05 11:45:20 -0700

    second commit

---
+++ Js/project.js
@@ -0,0 +1,13 @@
+
+
+function Dorcas() {
+    let name = prompt(`what is your name?`);
+    if (name == "") {
+        alert("Please enter a valid name")
+    } else {
+        alert("Thank you, we have received your information. " + name)
+    }
+}
+
+
+Dorcas();
